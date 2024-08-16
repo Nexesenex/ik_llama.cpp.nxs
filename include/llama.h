ad5de237a94c6944c08999eb009cdd762fe1d96d
Minsoo Cheong <icycle0409@snu.ac.kr>
2024-08-16 15:35:18 +0900

    llama : add EXAONE model support (#9025)
    * add exaone model support
    
    * add chat template
    
    * fix whitespace
    
    * add ftype
    
    * add exaone pre-tokenizer in `llama-vocab.cpp`
    
    * fix lint
    
    * add `EXAONE` to supported models in `README.md`
    
    * fix space
    
    ---------
    
    Co-authored-by: Georgi Gerganov <ggerganov@gmail.com>
    Co-Authored-By: compilade <113953597+compilade@users.noreply.github.com>
    Co-Authored-By: compilade <git@compilade.net>
    Co-Authored-By: Georgi Gerganov <ggerganov@gmail.com>

--- include/llama.h
+++ include/llama.h
@@ -95,7 +95,8 @@ extern "C" {
         LLAMA_VOCAB_PRE_TYPE_CODESHELL      = 22,
         LLAMA_VOCAB_PRE_TYPE_BLOOM          = 23,
         LLAMA_VOCAB_PRE_TYPE_GPT3_FINNISH   = 24,
-        LLAMA_VOCAB_PRE_TYPE_DEEPSEEK3_LLM  = 25, //llama.cpp lists this as 28
+        LLAMA_VOCAB_PRE_TYPE_EXAONE         = 25,
+        LLAMA_VOCAB_PRE_TYPE_DEEPSEEK3_LLM  = 26, //llama.cpp lists this as 28
     };
 
     enum llama_rope_type {
