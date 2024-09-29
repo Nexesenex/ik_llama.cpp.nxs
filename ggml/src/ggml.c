97b004ebd7986a397cb6e133657656fa182e3eb4
Georgi Gerganov <ggerganov@gmail.com>
2024-09-29 21:18:23 +0300

    ggml : define missing HWCAP flags (#9684)
    ggml-ci
    
    Co-authored-by: Willy Tarreau <w@1wt.eu>

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -4829,6 +4829,10 @@ static inline int ggml_up(int n, int m) {
 #include <sys/sysctl.h>
 #endif
 
+#if !defined(HWCAP2_I8MM)
+#define HWCAP2_I8MM 0
+#endif
+
 static void ggml_init_arm_arch_features(void) {
 #if defined(__linux__) && defined(__aarch64__)
     uint32_t hwcap = getauxval(AT_HWCAP);
