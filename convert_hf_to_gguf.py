7dcfa6433a143813a42a3dcc789621f50eccf022
nopperl <54780682+nopperl@users.noreply.github.com>
2024-09-29 12:02:06 +0000

    py : add model class for Chameleon conversion (#9683)

--- convert_hf_to_gguf.py
+++ convert_hf_to_gguf.py
@@ -4248,7 +4248,8 @@ class GraniteMoeModel(GraniteModel):
         return super().modify_tensors(data_torch, name, bid)
 
 
-@Model.register("ChameleonForCausalLM")
+@Model.register("ChameleonForConditionalGeneration")
+@Model.register("ChameleonForCausalLM")  # obsolete
 class ChameleonModel(Model):
     model_arch = gguf.MODEL_ARCH.CHAMELEON
 
