3b29632b51fdca2f7b17de76646a978cbce11403
Johannes Gäßler <johannesg@5d6.de>
2024-10-18 09:24:44 +0200

    CUDA: fix 1D im2col, add tests (ggml/993)

--- ggml/src/ggml-cuda.cu
+++ ggml/src/ggml-cuda.cu
@@ -3368,7 +3368,6 @@ GGML_CALL static bool ggml_backend_cuda_supports_op(ggml_backend_t backend, cons
         //case GGML_OP_ROPE:
         //    return ggml_is_contiguous(op->src[0]);
         case GGML_OP_IM2COL:
-            return op->src[0]->type == GGML_TYPE_F16;
         case GGML_OP_POOL_2D:
         case GGML_OP_SUM:
         case GGML_OP_SUM_ROWS:
