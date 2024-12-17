c70e045e0b2e1490e161e3acfd9a671051068ede
Billel Mokeddem <billel.mokeddem.ml@gmail.com>
2024-12-17 19:24:56 +0400

    llama : add Falcon3 support (#10864)

--- src/llama.cpp
+++ src/llama.cpp
@@ -6688,6 +6688,11 @@ static void llm_load_vocab(
                     tokenizer_pre == "falcon") {
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_FALCON;
             } else if (
+                    tokenizer_pre == "falcon3") {
+                vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_LLAMA3;
+                vocab.tokenizer_ignore_merges = true;
+                vocab.tokenizer_add_bos = true;
+            } else if (
                     tokenizer_pre == "mpt") {
                 vocab.type_pre = LLAMA_VOCAB_PRE_TYPE_MPT;
             } else if (
