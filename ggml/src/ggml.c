5f2ea7fa09b06e66f50bd10bd48a5268bd9670cb
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-03-07 10:58:38 +0100

    Update ggml.c

--- ggml/src/ggml.c
+++ ggml/src/ggml.c
@@ -18308,7 +18308,7 @@ static void ggml_compute_forward_flash_attn_ext_f16(
                 Dk, Dv, neq1, nek1, q->nb[1], k->nb[1], v->nb[1], mask->nb[1],
                 q->data, k->data, v->data, mask->data,
                 scale, softcap, (float *)dst->data,
-                params->wdata, (barrier_t)ggml_barrier, (void *)params->shared, ith, nth)) return;
+                params->wdata, (barrier_t)ggml_barrier, (void *)params->threadpool, ith, nth)) return;
 
 //    if (max_bias <= 0.0f && q->type == GGML_TYPE_F32 && mask && mask->type == GGML_TYPE_F16) {
 //        //if (ith == 0) printf("k: %ld x %ld x %ld, q: %ld x %ld x %ld, v: %ld x %ld x %ld mask: %ld x %ld x %ld\n",
