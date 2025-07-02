4e247d49ae33755f052c61d096d6faf87d17748a
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-07-02 18:17:49 +0200

    Maintenance of PR_91 (CLI - Specify GGML_TYPE to quantize the main tensors)
    - fix attn_v argument
    - add the CLI argument for _shexp.weight.
    - add _shexp.weight in the general tree of the quant strategies.
    - Make the PR effective for the IQ1 and IQ2 FTYPEs.

--- examples/quantize/quantize.cpp
+++ examples/quantize/quantize.cpp
@@ -148,7 +148,7 @@ static bool try_parse_ftype(const std::string & ftype_str_in, llama_ftype & ftyp
 //
 [[noreturn]]
 static void usage(const char * executable) {
-    printf("usage: %s [--help] [--allow-requantize] [--leave-output-tensor] [--pure] [--imatrix] [--hide-imatrix] [--include-weights] [--exclude-weights] [--output-tensor-type] [--token-embedding-type] [--attn-q-type] [--attn-k-type] [--attn-v-type] [--attn-qkv-type] [--attn-output-type] [--ffn-gate-type] [--ffn-down-type] [--ffn-up-type] [--keep-split] [--override-kv] model-f32.gguf [model-quant.gguf] type [nthreads]\n\n", executable);
+    printf("usage: %s [--help] [--allow-requantize] [--leave-output-tensor] [--pure] [--imatrix] [--hide-imatrix] [--include-weights] [--exclude-weights] [--output-tensor-type] [--token-embedding-type] [--attn-q-type] [--attn-k-type] [--attn-v-type] [--attn-qkv-type] [--attn-output-type] [--ffn-gate-type] [--ffn-down-type] [--ffn-up-type] [--shexp-type] [--keep-split] [--override-kv] model-f32.gguf [model-quant.gguf] type [nthreads]\n\n", executable);
     printf("  --allow-requantize: Allows requantizing tensors that have already been quantized. Warning: This can severely reduce quality compared to quantizing from 16bit or 32bit\n");
     printf("  --leave-output-tensor: Will leave output.weight un(re)quantized. Increases model size but may also increase quality, especially when requantizing\n");
     printf("  --pure: Disable k-quant mixtures and quantize all tensors to the same type\n");
@@ -169,7 +169,8 @@ static void usage(const char * executable) {
     printf("      --attn-output-type ggml_type: use this ggml_type for the attn_output.weight tensor.\n");
     printf("      --ffn-gate-type ggml_type: use this ggml_type for the ffn_gate tensor.\n");
     printf("      --ffn-down-type ggml_type: use this ggml_type for the ffn_down tensor.\n");
-    printf("      --ffn-up-type ggml_type: use this ggml_type for the ffn_up tensor.\n\n");
+    printf("      --ffn-up-type ggml_type: use this ggml_type for the ffn_up tensor.\n");
+    printf("      --shexp-type ggml_type: use this ggml_type for the _shexp.weight tensor.\n\n");
     printf("  --keep-split: will generate quantized model in the same shards as input\n");
     printf("  --override-kv KEY=TYPE:VALUE\n");
     printf("      Advanced option to override model metadata by key in the quantized model. May be specified multiple times.\n\n");
@@ -417,6 +418,12 @@ int main(int argc, char ** argv) {
         } else if (strcmp(argv[arg_idx], "--ffn-up-type") == 0) {
             if (arg_idx < argc-1) {
                 params.ffn_up_type = parse_ggml_type(argv[++arg_idx]);
+            } else {
+                usage(argv[0]);
+            }
+        } else if (strcmp(argv[arg_idx], "--shexp-type") == 0) {
+            if (arg_idx < argc-1) {
+                params.shexp_type = parse_ggml_type(argv[++arg_idx]);
             } else {
                 usage(argv[0]);
             }
