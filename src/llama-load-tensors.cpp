a4e66a92508c88061fe9fafdc22a79ad864739f1
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2025-12-22 00:03:22 +0100

    Update llama-load-tensors.cpp

--- src/llama-load-tensors.cpp
+++ src/llama-load-tensors.cpp
@@ -397,9 +397,9 @@ void create_tensors_helper::create_std_ffn(int i, const LLM_TN & tn, llama_layer
 bool create_tensors_helper::create_llama_tensors(const LLM_TN & tn) {
     LOADING_PRELUDE
 
-    if (cparams.split_output_tensor == 1) {
-        create_embd_output(tn, n_embd, n_vocab, true, true);
-    } else create_embd_output(tn, n_embd, n_vocab, true, false);
+    // if (cparams.split_output_tensor == 1) {
+    create_embd_output(tn, n_embd, n_vocab, true, true);
+    // } else create_embd_output(tn, n_embd, n_vocab, true, false);
 
     for (int i = 0; i < n_layer; ++i) {
         ggml_context * ctx_layer = ctx_for_layer(i);
@@ -1896,9 +1896,9 @@ bool create_tensors_helper::create_glm4_moe_tensors(const LLM_TN & tn) {
     GGML_ASSERT(hparams.n_expert > 0 && "n_expert must be > 0 for GLM4_MOE MoE layers");
     GGML_ASSERT(hparams.n_expert_used > 0 && "n_expert_used must be > 0 for GLM4_MOE MoE layers");
 
-    if (cparams.split_output_tensor == 1) {
-        create_embd_output(tn, n_embd, n_vocab, true, true);
-    } else create_embd_output(tn, n_embd, n_vocab, true, false);
+    // if (cparams.split_output_tensor == 1) {
+    create_embd_output(tn, n_embd, n_vocab, true, true);
+    // } else create_embd_output(tn, n_embd, n_vocab, true, false);
 
     for (int i = 0; i < n_layer; ++i) {
         ggml_context * ctx_layer = ctx_for_layer(i);
