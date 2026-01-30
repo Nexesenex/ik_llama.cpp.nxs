d63d2736b770465413d86b297c5a690273448b57
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-01-30 14:56:37 +0100

    Merge IKL's Support GLM-4.7-Flash-57B
    Make HuiHui work
    
    CPU flash attention
    
    CUDA FA

--- src/llama-build-context.cpp
+++ src/llama-build-context.cpp
@@ -7672,9 +7672,9 @@ ggml_cgraph * llm_build_context::build_deepseek2() {
                             }
 
                             ggml_tensor * kq = ggml_mul_mat(ctx0, kv_cache, q);
-                            if (kv_cache->ne[1] < 256) {
+                            //if (kv_cache->ne[1] < 256) {
                                 ggml_mul_mat_set_prec(kq, GGML_PREC_F32);
-                            }
+                            //}
                             cb(kq, "kq", il);
 
                             if (!pp_opt) {
@@ -7691,6 +7691,7 @@ ggml_cgraph * llm_build_context::build_deepseek2() {
                             }
 
                             kqv_compressed = ggml_mul_mat(ctx0, kv_cache_trans, kq);
+                            ggml_mul_mat_set_prec(kqv_compressed, GGML_PREC_F32);
                             cb(kqv_compressed, "kqv_compressed", il);
 
                             if (!pp_opt) {
