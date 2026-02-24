60669e0cd67a669ae256642e779ad162a40dde97
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-02-24 20:17:29 +0100

    Addition of recurrent models where it was missing due to reverts

--- src/llama-model.cpp
+++ src/llama-model.cpp
@@ -1788,3 +1788,15 @@ const char * llama_model_type_name(e_model type) {
         default:                  return "?B";
     }
 }
+
+bool llama_model_is_recurrent(const llama_model * model) {
+    return llm_arch_is_recurrent(model->arch);
+}
+
+bool llama_model_is_hybrid(const llama_model * model) {
+    return llm_arch_is_hybrid(model->arch);
+}
+
+bool llama_model_has_recurrent(const llama_model * model) {
+    return llm_arch_is_hybrid(model->arch) || llm_arch_is_recurrent(model->arch);
+}
