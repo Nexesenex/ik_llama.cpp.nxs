d15f021217dfba8028a49db716d56a03a7065c73
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-04 19:39:12 +0100

    Revert "affine tensor split handling in split mode graph."
    This reverts commit 603411bb57aa7cdcede249468b82deaf46c48f79.

--- src/llama-model.h
+++ src/llama-model.h
@@ -370,11 +370,10 @@ struct llama_model {
 
     std::vector<llama_layer> layers;
 
-llama_split_mode split_mode;
+    llama_split_mode split_mode;
     int main_gpu;
     int max_gpu = 0; // max. number of GPUs to use per layer for aplit mode "graph"
     int n_gpu_layers;
-    bool user_provided_tensor_split = false; // true if --tensor-split was explicitly provided
 
     bool mtp; // use mtp if is supported by the Model
 
