ae2c1da68e6bc528f9bcbdbb9462da0adf3d474b
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-04 20:49:27 +0100

    Add parameter --kv-split to choose on which GPU(s) the KV cache goes.

--- common/common.h
+++ common/common.h
@@ -226,6 +226,7 @@ struct gpt_params {
     int32_t main_gpu              =       0; // the GPU that is used for scratch and small tensors
     int32_t max_gpu               =       0; // max number of GPUs to use at a time for split mode "graph"
     float   tensor_split[128]     =     {0}; // how split tensors should be distributed across GPUs
+    float   kv_split[128]         =     {0}; // how split KV cache should be distributed across GPUs
     int32_t grp_attn_n            =       1; // group-attention factor
     int32_t grp_attn_w            =     512; // group-attention width
     int32_t n_print               =      -1; // print token count every n tokens (-1 = disabled)
