09293797f3bcb24b6e68d6ab785a771c37ec4ebd
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-06 22:11:10 +0100

    Rename split graph into split tensor parallel (MAIN)

--- common/common.h
+++ common/common.h
@@ -364,7 +364,7 @@ struct gpt_params {
     bool k_cache_hadamard  = false; // if true, use Hadamard transform for the K-cache (only makes sense with quantized cache)
     bool v_cache_hadamard  = false; // if true, use Hadamard transform for the V-cache (only makes sense with quantized cache, which requires FA)
     bool split_output_tensor = false; // if true, force split the output tensor in split mode graph
-    bool split_mode_graph_scheduling = false; // if true, force split mode graph scheduling
+    bool split_mode_tensor_parallel_scheduling = false; // if true, force split mode graph scheduling
     //bool split_mode_f16    = true;  // if true, intermediate results will be cast to f16 before copying to other GPUs to perform reduce ops
     bool scheduler_async   = false; // if true, in split mode graph the scheduler will use multiple threads to evaluate the graph
     int  fused_delta_net   = 0;     // use fused delta-net if number of tokens in the batch is less than this value
