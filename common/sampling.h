e14c105703ae834fddadbe252d521ce2ac1a0eba
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-14 19:54:36 +0100

    Revert "Adaptive P sampler: update review logic, delete old code comments, put prep stage after logit bias (#1386)"
    This reverts commit be2940f57a1adb7f27c6bd7816d3fb3188e62475.

--- common/sampling.h
+++ common/sampling.h
@@ -134,6 +134,8 @@ struct common_sampler {
     llama_token_data_array cur_p; // current candidates
 
     std::mt19937 rng;
+
+    int32_t n_rewind;   // number of tokens to rewind
 };
 
 
@@ -150,7 +152,7 @@ void common_sampler_reset(common_sampler * ctx);
 
 // Review stateful samplers
 // - rewind internal states (maybe)
-void common_sampler_review(common_sampler * ctx, const size_t n_unsent, const bool rewind_status);
+void common_sampler_review(common_sampler * ctx);
 
 // Set the sampler seed
 void llama_sampling_set_rng_seed(struct common_sampler * ctx, uint32_t seed);
