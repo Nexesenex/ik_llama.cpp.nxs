1af3c27eeb1502542e295cb6431c0c11c7a247b3
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-16 23:29:25 +0100

    Rename --max-gpu into --max-gpu-per-split

--- src/llama-model.h
+++ src/llama-model.h
@@ -558,7 +558,7 @@ struct llama_model {
 
     llama_split_mode split_mode;
     int main_gpu;
-    int max_gpu = 0; // max. number of GPUs to use per layer for aplit mode "graph"
+    int max_gpu_per_split = 0; // max. number of GPUs to use per layer for aplit mode "graph"
     int n_gpu_layers;
     int split_output_tensor = 0;  // 0=off, 1=split on all GPUs, N>1=split on top N GPUs by VRAM
     bool mtp; // use mtp if is supported by the Model
