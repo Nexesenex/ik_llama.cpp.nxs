d98b2e87bea65d03aec0c41029ca385fe03d20b0
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-16 23:29:25 +0100

    Rename --max-gpu into --max-gpu-per-split

--- include/llama.h
+++ include/llama.h
@@ -369,7 +369,7 @@ extern "C" {
         // LLAMA_SPLIT_ROW: the GPU that is used for small tensors and intermediate results
         // LLAMA_SPLIT_LAYER: ignored
         int32_t main_gpu;
-        int32_t max_gpu;
+        int32_t max_gpu_per_split;
         int32_t ncmoe;
 
         // proportion of the model (layers or rows) to offload to each GPU, size: llama_max_devices()
