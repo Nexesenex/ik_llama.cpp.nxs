eb1079aff995c113a85601c66b7c399b7bba3608
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-17 12:32:59 +0100

    Revert "Merge remote-tracking branch 'ik_llama_thireus.cpp/th/quantize_individual_tensors' into custom_naked_50"
    This reverts commit 11619a7e6c1aec76562201b3b5c844c8a1034298, reversing
    changes made to ce840110aa3c458b3f1a8e2708470b5f4b522178.

--- src/llama.cpp
+++ src/llama.cpp
@@ -2536,7 +2536,7 @@ static int llama_model_load(const std::string & fname, llama_model & model, llam
     try {
         llama_model_loader ml(fname, params.ncmoe, params.use_mmap, params.check_tensors,
                 params.repack_tensors, params.use_thp, params.merge_qkv, params.merge_up_gate_exps,
-                params.kv_overrides, params.tensor_buft_overrides, nullptr);
+                params.kv_overrides, params.tensor_buft_overrides);
 
         model.hparams.vocab_only = params.vocab_only;
 
@@ -4662,7 +4662,6 @@ struct llama_model_quantize_params llama_model_quantize_default_params() {
         /*.only_copy                   =*/ false,
         /*.pure                        =*/ false,
         /*.keep_split                  =*/ false,
-        /*.skip_first_shard            =*/ false,
         /*.ignore_imatrix_rules        =*/ false,
         /*.only_repack                 =*/ false,
         /*.dry_run                     =*/ false,
