a116e40066e2c33a35e4cca1a670e90a94b6fd8e
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-03-23 08:56:33 +0100

    Labelizing predicted sizes line

--- src/llama.cpp
+++ src/llama.cpp
@@ -2190,7 +2190,7 @@ static std::pair<std::vector<double>, double> get_layer_sizes(const llama_model_
     result[n_layer] += output_size;
     tot_cache += output_size;
     LLAMA_LOG_INFO("--------------------------------------------------------------------------\n");
-    LLAMA_LOG_INFO("Total   : %9.2f, %9.2f, %9.2f MiB\n", tot_model/1024./1024., tot_cache/1024./1024., (tot_model + tot_cache)/1024./1024.);
+    LLAMA_LOG_INFO("Sizes   : %9.2f MiB Model, %9.2f MiB Predicted KV Cache, %9.2f MiB Predicted Total\n", tot_model/1024./1024., tot_cache/1024./1024., (tot_model + tot_cache)/1024./1024.);
     return std::make_pair(std::move(result), max_compute);
 }
 
