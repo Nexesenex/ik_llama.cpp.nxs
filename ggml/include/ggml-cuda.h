575bbc1c5bb8e92bc097ad41bc3d931c0a206551
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-04-01 06:36:46 +0200

    Update ggml-cuda.h

--- ggml/include/ggml-cuda.h
+++ ggml/include/ggml-cuda.h
@@ -79,7 +79,7 @@ GGML_API GGML_CALL bool ggml_backend_cuda_get_pin_token_embd_only(void);
 GGML_API GGML_CALL bool ggml_backend_cuda_register_host_buffer(void * buffer, size_t size);
 GGML_API GGML_CALL void ggml_backend_cuda_unregister_host_buffer(void * buffer);
 
-GGML_API void ggml_backend_cuda_log_set_callback(ggml_log_callback log_callback, void * user_data);
+GGML_API GGML_CALL void ggml_backend_cuda_log_set_callback(ggml_log_callback log_callback, void * user_data);
 #ifdef  __cplusplus
 }
 #endif
