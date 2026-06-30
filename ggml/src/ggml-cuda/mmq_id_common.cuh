513feabc95ab77bb3bad6b8141a8fcfe7481e659
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-06-30 20:36:43 +0200

    CUDA - MMQ/MMQ_ID - Q4_1 - Loop unrolling for k01 outer loops in dp4a kernels

--- ggml/src/ggml-cuda/mmq_id_common.cuh
+++ ggml/src/ggml-cuda/mmq_id_common.cuh
@@ -709,7 +709,7 @@ static __device__ __forceinline__ void vec_dot_q4_1_q8_1_dp4a(
     const int   * y_qs = (const int   *) y + 4;
     const half2 * y_ds = (const half2 *) y;
 
-// #pragma unroll
+#pragma unroll
     for (int k01 = 0; k01 < MMQ_TILE_NE_K; k01 += QR4_1*VDR_Q4_1_Q8_1_MMQ) {
         const int k0 = k00 + k01;
 
