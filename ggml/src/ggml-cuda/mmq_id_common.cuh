1bce4560de52f846c5b9c49a73cc4feb3ab0850f
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-06-30 20:40:35 +0200

    IQ2_XS - Hoist division in load_tiles for DP4A path

--- ggml/src/ggml-cuda/mmq_id_common.cuh
+++ ggml/src/ggml-cuda/mmq_id_common.cuh
@@ -2883,8 +2883,10 @@ template <int mmq_y, bool need_check> static __device__ __forceinline__ void loa
         x_df[i*MMQ_MMA_TILE_X_K_Q3_K                   + 2*kqsx+0] = ((ls &  0x0F)*d + d/2)/4;
         x_df[i*MMQ_MMA_TILE_X_K_Q3_K                   + 2*kqsx+1] = ((ls >>    4)*d + d/2)/4;
 #else
-        x_df[i*(2*MMQ_TILE_NE_K*2/QI8_0) + i/(QI8_0/4) + 2*kqsx+0] = ((ls &  0x0F)*d + d/2)/4;
-        x_df[i*(2*MMQ_TILE_NE_K*2/QI8_0) + i/(QI8_0/4) + 2*kqsx+1] = ((ls >>    4)*d + d/2)/4;
+        constexpr int blocks_per_tile_x_row_iq2_xs = 2*MMQ_TILE_NE_K*2/QI8_0;
+        const int i_dm_iq2_xs = i/(QI8_0/4);
+        x_df[i*blocks_per_tile_x_row_iq2_xs + i_dm_iq2_xs + 2*kqsx+0] = ((ls &  0x0F)*d + d/2)/4;
+        x_df[i*blocks_per_tile_x_row_iq2_xs + i_dm_iq2_xs + 2*kqsx+1] = ((ls >>    4)*d + d/2)/4;
 #endif // defined(AMD_MFMA_AVAILABLE) || defined(TURING_MMA_AVAILABLE)
     }
 }
