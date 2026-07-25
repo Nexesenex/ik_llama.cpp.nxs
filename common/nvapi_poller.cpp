a9b25e29bcabd65977e0bc0afc32bce1454a9aed
Nexesenex <124105151+Nexesenex@users.noreply.github.com>
2026-07-25 08:40:57 +0200

    Update nvapi_poller.cpp

--- common/nvapi_poller.cpp
+++ common/nvapi_poller.cpp
@@ -73,6 +73,7 @@ void NvapiPoller::thread_func() {
             handles.size(), interval_ms);
 
     static char* d_bufs[64] = {};
+    static cudaStream_t d_streams[64] = {};
 
     while (should_run.load()) {
         for (size_t i = 0; i < handles.size(); ++i) {
@@ -94,14 +95,14 @@ void NvapiPoller::thread_func() {
             pstates20.version = NV_GPU_PERF_PSTATES20_INFO_VER;
             NvAPI_GPU_GetPstates20(handle, &pstates20);
 
+            cudaSetDevice(devices[i]);
             if (d_bufs[i] == nullptr) {
-                cudaSetDevice(devices[i]);
+                cudaStreamCreateWithFlags(&d_streams[i], cudaStreamNonBlocking);
                 cudaMalloc(&d_bufs[i], 4096);
             }
-            cudaSetDevice(devices[i]);
-            cudaMemset(d_bufs[i], 0, 4096);
-            cudaMemset(d_bufs[i], 0xFF, 4096);
-            cudaDeviceSynchronize();
+            cudaMemsetAsync(d_bufs[i], 0, 4096, d_streams[i]);
+            cudaMemsetAsync(d_bufs[i], 0xFF, 4096, d_streams[i]);
+            cudaStreamSynchronize(d_streams[i]);
         }
         std::this_thread::sleep_for(std::chrono::milliseconds(interval_ms));
     }
@@ -109,6 +110,7 @@ void NvapiPoller::thread_func() {
     for (size_t i = 0; i < handles.size(); ++i) {
         if (d_bufs[i] != nullptr) {
             cudaSetDevice(devices[i]);
+            cudaStreamDestroy(d_streams[i]);
             cudaFree(d_bufs[i]);
         }
     }
